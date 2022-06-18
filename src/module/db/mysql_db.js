6ab9473ff61f9c73de185db6741938b727c79f52
chengjiang_09 <chengjiang_09@163.com>
2022-06-18 16:46:59 +0800

    '修改数据库配置'

--- src/module/db/mysql_db.js
+++ src/module/db/mysql_db.js
@@ -5,7 +5,7 @@ export const getMysqlPool = () => {
         host:'127.0.0.1',
         port:3306,
         user:'root',
-        password:'1314520yaNG',
+        password:'',
         database:'center_db'
     })
 }
